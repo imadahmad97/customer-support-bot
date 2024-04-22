e9861e6b9b351464706b9368d5648d7e3fd9f486
Imad Ahmad <imadahmad97@yahoo.ca>
2024-04-21 17:18:10 -0700

    styled chatbot

--- app.py
+++ app.py
@@ -1,30 +1,42 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-genai.configure(api_key=GOOGLE_API_KEY)
 app = Flask(__name__)
-CORS(app)
 
-model = genai.GenerativeModel('gemini-pro')
+def configure_model():
+    print("Configuring model with API Key:", GOOGLE_API_KEY)
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    return model
+
+model = configure_model()
 
 global_context = """
-This is a customer service bot designed to assist with inquiries about products and services offered by [Your Company Name]. It should provide accurate information, demonstrate patience, and maintain a polite and professional tone at all times. The bot should escalate issues it cannot resolve to human agents.
+This is a customer service bot designed to assist with inquiries about products and services offered by [Your Company Name].
 """
 
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    user_input = request.json.get('user_input')
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get", methods=["POST"])
+def chat():
+    print("Received data:", request.json)
+    user_input = request.json.get('msg')
     if not user_input:
         return jsonify({'error': 'No user input provided'}), 400
+    return get_Chat_response(user_input)
 
-    prompt = global_context + "\nCustomer: " + user_input + "\nBot:"
-    response = model.generate_content(prompt)
-    return jsonify({'response': response})
+def get_Chat_response(text):
+    prompt = global_context + "\nCustomer: " + text + "\nBot:"
+    response_object = model.generate_content(prompt)
+    response_text = response_object.result['candidates'][0]['content']['parts'][0]['text']
+    return jsonify({'response': response_text})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
